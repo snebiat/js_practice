0f6818648baf7ea77914fb14cd76720deb9b3a7b
Ibssa Nebiat <s2200731@BCC02J6CQ6LRAir.home>
2022-09-04 23:04:21 +0300

    adjust folders and add lesson0509

---
+++ Practice_lessons0509/Implicitreturn/implicit01.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+Re-write the function triple without using the keyword return.
+
+
+/**
+ * @param {number} value
+ */
+function triple () {
+
+}
+
+
+// Sample usage - do not modify
+console.log(triple(2)); // 6
+console.log(triple(3)); // 9
---
+++ Practice_lessons0509/Implicitreturn/implicit03.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+Complete the function getPositiveTemperatures such that it returns an array containing the positive temperatures (the temperatures that are above 0).
+Use an arrow function (implicit return is optional).
+/**
+ * @param {number} value
+ */
+function getPositiveTemperatures () {
+
+}
+
+
+// Sample usage - do not modify
+console.log(multiply(2, 4)); // 8
+console.log(multiply(3, 3)); // 9
